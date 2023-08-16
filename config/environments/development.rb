dec021a5d5e5614fb966d9e80dbb7d283861437c
Dave Millican <dave@skillerwhale.com>
2023-08-16 12:04:10 +0100

    Update host environment for HLE

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  if ENV['SW_HOSTNAME'].present?
+    config.hosts << ENV['SW_HOSTNAME']
+  end
 end
